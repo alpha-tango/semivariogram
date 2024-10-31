1cc91fb424c67c0631bcd0cc14edd777d733ff6d
alpha-tango <alpha.tango.pope@gmail.com>
2024-10-31 10:22:58 -0400

    + WIP kriging

--- driver.py
+++ driver.py
@@ -5,14 +5,14 @@ import importlib
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from scipy.spatial import distance_matrix
 from typing import Union, List
 
 import scripts.binner as binners
 import scripts.models as models
 import scripts.plots as plots
 import scripts.stats as stats
-
-import config.berea as workflow_config
 
 
 def main():
@@ -38,7 +38,7 @@ def main():
     
     parser.add_argument(
         'plot',
-        choices=['raw_histogram', 'raw_semivariogram', 'semivariogram'],
+        choices=['raw_histogram', 'raw_semivariogram', 'semivariogram', 'kriging'],
         help="""
         Specify which plot you want to build.
         Raw histogram displays a histogram of the raw data.
@@ -57,7 +57,7 @@ def main():
         """
         )
 
-    parser.add_argument("--bin_width", type=int, 
+    parser.add_argument("--bin_width", type=float, 
         help="""
         If set, this forces the semivariogram to use the specified, fixed bin width.
         """)
@@ -75,7 +75,7 @@ def main():
         """)
 
     parser.add_argument("--model",
-        choices=['spherical', 'exponential', 'gaussian', 'isotonic'],
+        choices=['exponential', 'hw6'],
         help="""
         Select a model to fit the semivariogram on, when used in 'semivariogram' mode.
         Choose a single model or 'all'. Currently must be used with "--range" setting.
@@ -94,8 +94,6 @@ def main():
 
     if options.bin_method == 'fixed_width':
         assert options.bin_width is not None
-    elif options.bin_method == 'equal_points':
-        assert options.points_per_bin is not None
     elif options.bin_method == 'config':
         assert workflow_config.BIN_MAXES is not None
 
@@ -105,6 +103,7 @@ def main():
 
     # get the raw data from the config file
     raw_df = workflow_config.raw_data()
+    print(raw_df.head())
     sample_count = raw_df['id'].count()
     print(f"{sample_count} samples found in raw data")
 
@@ -213,9 +212,9 @@ def main():
         binner = binners.EqualPointBinner(points_per_bin=options.points_per_bin)
         bins_df = binner.bins(pair_df=pair_df)
 
-    else:  # options.bin_method == 'raw' based on the argument options
-        print('not supported')
-        return 1
+    elif options.bin_method == 'raw':
+        binner = binners.RawBinner()
+        bins_df = binner.bins(pair_df=pair_df)
 
     # check work
     print(bins_df.head(22))
@@ -257,35 +256,116 @@ def main():
 
     # fit the model to the data
     model_class = models.get_model(options.model)
-    model = model_class(data_df=to_model_df, fit_range=options.range)
-    print(model.name)
+    model = model_class(data_df=bins_df, fit_range=options.range)
 
     # make semivariogram
     if options.plot == 'semivariogram':
 
-        # fit and plot model
-        if options.model in ('spherical', 'exponential', 'gaussian', 'isotonic'):
+        plot_model = model.plottable()
+        plot = plots.Semivariogram(
+                    a=options.range,
+                    omega=model.sill,
+                    model_name=model.name,
+                    model_lag=plot_model['h'],
+                    model_semivariance=plot_model['semivariance'],
+                    raw_df=bins_df,
+                    imname=workflow_config.IM_TAG
+                    )
 
-            plot_model = model.plottable()
-            plot = plots.Semivariogram(
-                        a=options.range,
-                        omega=model.sill,
-                        model_name=model.name,
-                        model_lag=plot_model['h'],
-                        model_semivariance=plot_model['semivariance'],
-                        raw_df=bins_df,
-                        imname=workflow_config.IM_TAG
-                        )
-
-            plot.show_and_save()
-
-        else:
-            print("Model not implemented yet, sorry.")
-            return 1
+        plot.show_and_save()
 
         # TODO: plots overwrite each other, fix this.
-
         return 0
+
+    #############################################################
+    # Kriging
+    #############################################################
+
+    sample_coords = raw_df[['x', 'y']].values
+
+    # create distance matrix using scipy function
+    # it uses minkowski distance, p=2 is same as Euclidean distance
+    pair_matrix = distance_matrix(sample_coords, sample_coords, p=2)  #
+
+    # calculate semivariance from model and distance matrix
+    semivariance_knownpts_matrix = model.fit_h(pair_matrix)
+
+    # add the ones and zero as described in class (due to Lagrange Param)
+    b = np.ones((len(sample_coords),1))
+    q = np.ones((1, len(sample_coords) + 1))
+    C_semivariance_matrix = np.hstack((semivariance_knownpts_matrix,b))
+    C_semivariance_matrix = np.vstack((C_semivariance_matrix,q))
+    C_semivariance_matrix[len(sample_coords), len(sample_coords)] = 0
+
+    # construct a target grid
+    # get min and max coords from sample data so we know where to center our grid
+    min_x = raw_df['x'].min()
+    min_y = raw_df['y'].min()
+    max_x = raw_df['x'].max()
+    max_y = raw_df['y'].max()
+
+    # lets add a tolerance around the edges
+    x_tol = (max_x - min_x) * .1
+    y_tol = (max_y - min_y) * .1
+
+    # now make a 20 x 20 grid around the sample points
+    n_pts = 5
+    xs = np.linspace(min_x - x_tol, max_x + x_tol, n_pts)
+    ys = np.linspace(min_y - y_tol, max_y + y_tol, n_pts)
+    target_coords = [[i,j] for i in xs for j in ys]
+
+    # calculate the distances between the sample points and the target point
+    target_coords = [[7, 14]]
+
+    print("Vector Target Distance")
+    target_vector = distance_matrix(sample_coords, target_coords)
+    D_semivariance_target_matrix = model.fit_h(target_vector)
+    print("target vector shape", target_vector.shape)
+    print("D matrix shape (pre adding Lagrange)", D_semivariance_target_matrix.shape)
+
+    # Add one on to end of vector due to Lagrange Param
+    q = np.ones((1, len(target_coords)))
+    D_semivariance_target_matrix = np.vstack((D_semivariance_target_matrix,q))
+    print("D matrix shape (with Lagrange)", D_semivariance_target_matrix.shape)
+
+    # do the matrix operations to get the w matrix
+    C_inv = np.linalg.inv(C_semivariance_matrix)
+    W_weights_matrix = np.matmul(C_inv, D_semivariance_target_matrix)
+    print("W matrix shape", W_weights_matrix.shape)
+    # print(W_weights_matrix[:-1].sum())  # check sum to 1 (except Lagrange param) -- YES!
+
+    # get the values of the known samples points
+    actual_values_vector = raw_df[['primary']].values
+
+    # add in a zero to deal with the Lagrange param
+    q = np.zeros((1,1))
+    actual_values_vector = np.vstack((actual_values_vector, q))
+    actual_values_col = np.rot90(actual_values_vector)
+    print("actual values shape", actual_values_col.shape)
+
+    # use matrix multiplication to calculate estimated value
+    estimates = np.matmul(actual_values_col, W_weights_matrix)
+    print(estimates)
+    print("estimate matrix", estimates.shape)
+    
+    # # turn the coords and estimates into a dataframe
+    # data = np.hstack((target_coords, np.rot90(estimates)))
+    # data_df = pd.DataFrame(data, columns=['x','y', 'estimate'])
+
+
+    # fig, ax = plt.subplots()
+    # c = ax.pcolormesh(data_df['x'], data_df['y'], data_df['estimate'],
+    #     cmap='RdBu',
+    #     vmin=data_df['estimate'].min(),
+    #     vmax=data_df['estimate'].max())
+    # ax.set_title('pcolormesh')
+    # # set the limits of the plot to the limits of the data
+    # ax.axis([data_df['x'].min(), data_df['x'].max(), data_df['y'].min(), data_df['y'].max()])
+    # fig.colorbar(c, ax=ax)
+
+    # plt.show()
+
+    return 0
 
 
 if __name__ == "__main__":
